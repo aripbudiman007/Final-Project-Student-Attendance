b49b27da03635f7e8b124243bd3823dfa102be79
UmamKhoerul <umam.itdev@gmail.com>
2021-06-18 14:00:53 +0700

    tambah edit dan delete

--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -5,5 +5,9 @@ const router = require('express').Router()
 router.get('/', StudentController.home)
 router.get('/add', StudentController.formAddStudent)
 router.post('/add', StudentController.addStudent)
+router.get('/edit/:id', StudentController.updateStudentForm)
+router.post('/edit/:id', StudentController.updateStudentAction)
+router.get('/delete/:id', StudentController.deleteStudent)
+
 
 module.exports = router
